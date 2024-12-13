b7725803721a60c3303210b75b4aab73c7e4af6c
Alexander <alexander.ogorodov@gmail.com>
2024-12-14 03:01:25 +0400

    step 4: refactoring (again...)

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,8 +1,5 @@
 #!/usr/bin/env node
 
 import greetUser from "../src/cli.js";
-import runEvenGame from "../src/games/brain-even.js";
 
-console.log("Welcome to Brain Games!");
 greetUser();
-runEvenGame();
