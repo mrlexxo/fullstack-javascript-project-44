eb17baac621a48d1d6c28a8a95d187ef54f7e607
Alexander <alexander.ogorodov@gmail.com>
2024-12-16 03:00:17 +0400

    removed gettings in all games, fixed rules typos

--- src/index.js
+++ src/index.js
@@ -1,23 +1,22 @@
-import readlineSync from 'readline-sync';
+import readlineSync from "readline-sync";
 
 const roundCount = 3;
 
 const gameEngine = (gameRules, generateData) => {
-  console.log('Welcome to Brain Games!');
-  const playerName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${playerName}!`);
   console.log(gameRules);
 
   for (let i = 0; i < roundCount; i += 1) {
     const [questionEntry, correctAnswer] = generateData();
     console.log(`Question: ${questionEntry}`);
-    const userAnswer = readlineSync.question('Your answer: ').toLowerCase();
+    const userAnswer = readlineSync.question("Your answer: ").toLowerCase();
 
     if (correctAnswer !== userAnswer) {
-      console.log(`'${userAnswer}' is wrong answer. Correct answer was '${correctAnswer}'.`);
-      return console.log('Try again!');
+      console.log(
+        `'${userAnswer}' is wrong answer. Correct answer was '${correctAnswer}'.`
+      );
+      return console.log("Try again!");
     }
-    console.log('Correct answer!');
+    console.log("Correct answer!");
   }
   return console.log(`Well done, ${playerName}!`);
 };
