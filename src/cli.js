886c4f4aceb16953178e3203b86e5cfefabc7c51
Alexander <alexander.ogorodov@gmail.com>
2024-12-16 00:50:58 +0400

    step 8-1: added a small manual executed by brain-help command

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,12 @@
-import readlineSync from 'readline-sync';
+import readlineSync from "readline-sync";
 
 const greetUser = () => {
   console.log("Welcome to Brain Games!");
-  const name = readlineSync.question('May I have your name? ');
+  const name = readlineSync.question("May I have your name? ");
   console.log(`Hello, ${name}!`);
+  console.log(
+    `Type brain-help for a short manual on how this project operates.`
+  );
 };
 
 export default greetUser;
---
+++ src/games/cli-brain-help.js
@@ -0,0 +1,14 @@
+const runHelp = () => {
+  console.log(
+    "Hi! This is my test project containing a bunch of different games. Hope you'll enjoy it and give me job c:"
+  );
+  console.log("Type brain-calc for a calculation game.");
+  console.log("Type brain-even for an even-odd game.");
+  console.log(
+    "Type brain-gcd for finding the greatest common denominator game."
+  );
+  console.log("Type brain-calc for a progression game.");
+  console.log("Type brain-calc for a prime number game.");
+};
+
+export default runHelp;
