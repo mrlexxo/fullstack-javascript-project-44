dee3eed53475bc1730f5e4faddea1c8851838a8f
Alexander <alexander.ogorodov@gmail.com>
2024-12-16 01:11:11 +0400

    step 8: brain-prog game logic implemented

--- src/games/cli-brain-prog.js
+++ src/games/cli-brain-prog.js
@@ -2,6 +2,29 @@ import gameEngine from "../index.js";
 import getRandomNum from "../getRandomNum.js";
 
 const gameRules = "What number is missing in the progression?";
+const createProgression = (firstItem, progStep, progLength) => {
+  const progArray = [];
+
+  for (let i = 1; i < progLength; i += 1) {
+    const result = firstItem + progStep * i;
+    progArray.push(result);
+  }
+
+  return progArray;
+};
+
+const generateData = () => {
+  const firstItem = getRandomNum(1, 9);
+  const progStep = getRandomNum(1, 9);
+  const progLength = getRandomNum(5, 9);
+  const progGenerated = createProgression(firstItem, progStep, progLength);
+  const randomIndex = Math.floor(Math.random() * progGenerated.length);
+
+  const correctAnswer = `${progGenerated[randomIndex]}`;
+  progGenerated[randomIndex] = "..";
+  const questionEntry = progGenerated.join(" ");
+  return [questionEntry, correctAnswer];
+};
 
 const runProgGame = () => gameEngine(gameRules, generateData);
 
