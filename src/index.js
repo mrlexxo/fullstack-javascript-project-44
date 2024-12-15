e46288f8323c3e6216983a81a73efec9a4291fca
Alexander <alexander.ogorodov@gmail.com>
2024-12-16 03:14:59 +0400

    returned greeting and congratulations with a username

--- src/index.js
+++ src/index.js
@@ -1,24 +1,27 @@
-import readlineSync from 'readline-sync';
+import readlineSync from "readline-sync";
 
 const roundCount = 3;
 
 const gameEngine = (gameRules, generateData) => {
+  console.log("Welcome to the Brain Games!");
+  const userName = readlineSync.question("May I have your name? ");
+  console.log(`Hello, ${userName}!`);
   console.log(gameRules);
 
   for (let i = 0; i < roundCount; i += 1) {
     const [questionEntry, correctAnswer] = generateData();
     console.log(`Question: ${questionEntry}`);
-    const userAnswer = readlineSync.question('Your answer: ').toLowerCase();
+    const userAnswer = readlineSync.question("Your answer: ").toLowerCase();
 
     if (correctAnswer !== userAnswer) {
       console.log(
-        `'${userAnswer}' is wrong answer. Correct answer was '${correctAnswer}'.`,
+        `'${userAnswer}' is wrong answer. Correct answer was '${correctAnswer}'.`
       );
-      return console.log('Try again!');
+      return console.log(`Try again, ${userName}!`);
     }
-    console.log('Correct answer!');
+    console.log("Correct answer!");
   }
-  return console.log('Well done!');
+  return console.log(`Congratulations, ${userName}!`);
 };
 
 export default gameEngine;
