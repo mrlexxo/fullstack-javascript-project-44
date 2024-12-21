78f5731206c29bc3556d433de4702b55ba4a16d3
Alexander Ogorodov <95526039+mrlexxo@users.noreply.github.com>
2024-12-21 07:15:26 +0300

    Update getRandomNum.js
    correction of #1

--- src/getRandomNum.js
+++ src/getRandomNum.js
@@ -1,3 +1,3 @@
-const getRandomNum = (minNum, maxNum) => Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
+const getRandomNum = (minNum = 0, maxNum = 100) => Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
 
 export default getRandomNum;
