2d0e8ebc9febe3d26b0dc5f0efad25a22fee5b66
mrlexxo <alexander.ogorodov@gmail.com>
2024-12-24 15:54:24 +0400

    cli-brain-calc: error msg added

--- src/games/cli-brain-calc.js
+++ src/games/cli-brain-calc.js
@@ -1,19 +1,19 @@
-import gameEngine from '../index.js';
-import getRandomNum from '../getRandomNum.js';
+import gameEngine from "../index.js";
+import getRandomNum from "../getRandomNum.js";
 
-const operandArray = ['+', '-', '*'];
-const gameRules = 'What is the result of the expression?';
+const operandArray = ["+", "-", "*"];
+const gameRules = "What is the result of the expression?";
 
 const expression = (num1, operand, num2) => {
   switch (operand) {
-    case '+':
+    case "+":
       return num1 + num2;
-    case '-':
+    case "-":
       return num1 - num2;
-    case '*':
+    case "*":
       return num1 * num2;
     default:
-      return null;
+      throw new Error(`Invalid operator - ${operand}`);
   }
 };
 
@@ -21,7 +21,8 @@ const generateData = () => {
   const num1 = getRandomNum(1, 99);
   const num2 = getRandomNum(1, 99);
 
-  const operandRandomized = operandArray[getRandomNum(0, operandArray.length - 1)];
+  const operandRandomized =
+    operandArray[getRandomNum(0, operandArray.length - 1)];
   const questionEntry = `${num1} ${operandRandomized} ${num2}`;
   const correctAnswer = `${expression(num1, operandRandomized, num2)}`;
 
